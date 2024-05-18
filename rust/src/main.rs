ba43b54463439e8d25b9bb86ffef4f38c873a243
tariqsaoud11 <tariqsaoud11@gmail.com>
2024-05-18 00:02:40 -0400

    INV-44-backend-inventory-api: set for items and places

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -17,7 +17,7 @@ use serde_json::{Map, Number, Value};
 use sqlx::postgres::PgPoolOptions;
 use std::env;
 use tower_http::cors::CorsLayer;
-use uuid::Uuid;
+
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
@@ -33,7 +33,7 @@ async fn main() {
         // `POST /users` goes to `create_user`
         .route("/users", post(create_user))
         // 'get_/places' goes to `get_places`
-        // .route("/places", get(get_places))
+        .route("/places", get(get_places))
         // 'get_/items' goes to `get_items`
         .route("/items", get(get_items))
         //enable cors
@@ -53,26 +53,26 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn create_place(Json(payload): Json<CreatePlace>) -> impl IntoResponse {
-    let place = Place {
-        place_id: payload.place_id,
-        place_name: payload.place_name,
-        place_type: payload.place_type,
-    };
+// async fn create_place(Json(payload): Json<CreatePlace>) -> impl IntoResponse {
+//     let place = Place {
+//         place_id: payload.place_id,
+//         place_name: payload.place_name,
+//         place_type: payload.place_type,
+//     };
 
-    (StatusCode::CREATED, Json(place))
-}
+//     (StatusCode::CREATED, Json(place))
+// }
 
 
-async fn create_item(Json(payload): Json<CreateItem>) -> impl IntoResponse {
-    let item = Item {
-        item_id: Uuid::new_v4(),
-        place_id: payload.place_id,
-        nb_of_items: payload.nb_of_items,
-        item_name: payload.item_name, 
-    };
-    (StatusCode::CREATED, Json(item))
-}
+// async fn create_item(Json(payload): Json<CreateItem>) -> impl IntoResponse {
+//     let item = Item {
+//         item_id: Uuid::new_v4(),
+//         place_id: payload.place_id,
+//         nb_of_items: payload.nb_of_items,
+//         item_name: payload.item_name, 
+//     };
+//     (StatusCode::CREATED, Json(item))
+// }
 
 async fn create_user(
     // this argument tells axum to parse the request body
@@ -90,20 +90,20 @@ async fn create_user(
     (StatusCode::CREATED, Json(user))
 }
 
-// async fn get_places_db() -> anyhow::Result<Vec<Place>> {
-//     dotenv().ok();
-//     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-//     let pool = PgPoolOptions::new()
-//         .max_connections(5)
-//         .connect(&database_url)
-//         .await?;
+async fn get_places_db() -> anyhow::Result<Vec<Place>> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
     
-//     let places = sqlx::query_as!(Place, "SELECT placeId as place_id, placeName as place_name, placeType as place_type FROM Places")
-//     .fetch_all(&pool)
-//     .await?;
+    let places = sqlx::query_as!(Place, "SELECT placeId as place_id, placeName as place_name, placeType as place_type FROM Places")
+    .fetch_all(&pool)
+    .await?;
 
-//     Ok(places)
-// }
+    Ok(places)
+}
 
 async fn get_items_db() -> anyhow::Result<Vec<Item>> {
     dotenv().ok();
@@ -121,30 +121,30 @@ async fn get_items_db() -> anyhow::Result<Vec<Item>> {
 }
 
 
-// async fn get_places() -> impl IntoResponse {
-//     match get_places_db().await {
-//         Ok(places_result) => {
-//             let list_response: Vec<Value> = places_result
-//                 .into_iter()
-//                 .map(|place| {
-//                     let mut map = Map::new();
-//                     map.insert("placeId".to_string(), Value::String(place.place_id.to_string()));
-//                     map.insert("placeName".to_string(), Value::String(place.place_name));
-//                     map.insert("placeType".to_string(), Value::String(place.place_type));
-//                     Value::Object(map)
-//                 })
-//                 .collect();
+async fn get_places() -> impl IntoResponse {
+    match get_places_db().await {
+        Ok(places_result) => {
+            let list_response: Vec<Value> = places_result
+                .into_iter()
+                .map(|place| {
+                    let mut map = Map::new();
+                    map.insert("placeId".to_string(), Value::String(place.place_id.to_string()));
+                    map.insert("placeName".to_string(), Value::String(place.place_name));
+                    map.insert("placeType".to_string(), Value::String(place.place_type));
+                    Value::Object(map)
+                })
+                .collect();
 
-//             let obj = Value::Array(list_response);
-//             (StatusCode::OK, Json(obj))
-//         }
-//         Err(err) => {
-//             eprintln!("Error fetching places: {}", err);
-//             let error_response = serde_json::json!({ "error": err.to_string() });
-//             (StatusCode::INTERNAL_SERVER_ERROR, Json(error_response))
-//         }
-//     }
-// }
+            let obj = Value::Array(list_response);
+            (StatusCode::OK, Json(obj))
+        }
+        Err(err) => {
+            eprintln!("Error fetching places: {}", err);
+            let error_response = serde_json::json!({ "error": err.to_string() });
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(error_response))
+        }
+    }
+}
 
 
 
@@ -241,40 +241,34 @@ struct User {
     id: u64,
     username: String,
 }
-#[derive(Deserialize)]
-struct CreatePlace {
-    place_id: Uuid,
-    place_name: String,
-    place_type: String,
-}
+// #[derive(Deserialize)]
+// struct CreatePlace {
+//     place_id: u32,
+//     place_name: String,
+//     place_type: String,
+// }
 
 #[derive(Serialize)]
 struct Place {
-    place_id: Uuid,
+    place_id: i32,
     place_name: String,
     place_type: String,
 }
 
-#[derive(Deserialize)]
-struct CreateItem {
-    item_id: Uuid,
-    place_id: Uuid,
-    nb_of_items: String,
-    item_name: String,
-}
+// #[derive(Deserialize)]
+// struct CreateItem {
+//     item_id: u32,
+//     place_id: u32,
+//     nb_of_items: String,
+//     item_name: String,
+// }
 
 #[derive(Serialize)]
 struct Item {
-    item_id: Uuid,
-    place_id: Uuid,
+    item_id: i32,
+    place_id: i32,
     nb_of_items: String,
     item_name: String,
 }
 
-// #[derive(sqlx::Type, Deserialize, Serialize)]
-// #[sqlx(type_name = "placeTypeSelector")]
-// #[serde(rename_all = "snake_case")]
-// enum PlaceType {
-//     Out,
-//     In,
-// }
+
