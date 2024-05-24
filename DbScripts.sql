ce808bea9547a4b26ad3bd2c26e291b74f451cc3
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-05-24 18:45:05 -0400

    INV-44: update schemas db

--- DbScripts.sql
+++ DbScripts.sql
@@ -1,32 +1,30 @@
--- DROP TABLE IF EXISTS Items;
--- DROP TABLE IF EXISTS Places;
-
-CREATE TYPE placeTypeSelector AS ENUM ('OUT', 'IN');
+ DROP TABLE IF EXISTS Items;
+ DROP TABLE IF EXISTS Places;
 
 CREATE TABLE Places (
-    placeId UUID PRIMARY KEY NOT NULL,
+    placeId SERIAL PRIMARY KEY,
     placeName VARCHAR(30) NOT NULL,
-    placeType placeTypeSelector NOT NULL
+    placeType VARCHAR(3) NOT NULL
 );
 
 CREATE TABLE Items (
-    itemId UUID PRIMARY KEY NOT NULL,
-    placeId UUID REFERENCES Places(placeId) NOT NULL,
-    nbOfItems VARCHAR(30) NOT NULL, 
+    itemId SERIAL PRIMARY KEY,
+    placeId INT REFERENCES Places(placeId) NOT NULL,
+    nbOfItems INT not NULL, 
     itemName VARCHAR(30) NOT NULL
 );
 
-INSERT INTO Places (placeId, placeName, placeType) 
-VALUES (gen_random_uuid(), 'Inventaire', 'IN');
+INSERT INTO Places (placeName, placeType) 
+VALUES ('Inventaire', 'IN');
 
-INSERT INTO Items (itemId, placeId, nbOfItems, itemName) 
-VALUES (gen_random_uuid(), (SELECT placeId FROM Places WHERE placeName = 'Inventaire'), '3', 'chaise');
+INSERT INTO Items (placeId, nbOfItems, itemName) 
+VALUES ((SELECT placeId FROM Places WHERE placeName = 'Inventaire'), 3, 'chaise');
 
-INSERT INTO Items (itemId, placeId, nbOfItems, itemName) 
-VALUES (gen_random_uuid(), (SELECT placeId FROM Places WHERE placeName = 'Inventaire'), '2', 'hamac');
+INSERT INTO Items (placeId, nbOfItems, itemName) 
+VALUES ((SELECT placeId FROM Places WHERE placeName = 'Inventaire'), 2, 'hamac');
 
-INSERT INTO Items (itemId, placeId, nbOfItems, itemName) 
-VALUES (gen_random_uuid(), (SELECT placeId FROM Places WHERE placeName = 'Inventaire'), '1', 'ballon');
+INSERT INTO Items (placeId, nbOfItems, itemName) 
+VALUES ((SELECT placeId FROM Places WHERE placeName = 'Inventaire'), 1, 'ballon');
 
 SELECT * FROM Places;
 SELECT * FROM Items;
