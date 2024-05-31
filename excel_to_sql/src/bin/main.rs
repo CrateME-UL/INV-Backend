e02cd14cfabf813fe6bc5381a6e6d8ab0de09feb
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-05-31 16:44:08 -0400

    INV-50: roadmap for adding data

--- excel_to_sql/src/bin/main.rs
+++ excel_to_sql/src/bin/main.rs
@@ -15,12 +15,15 @@ fn main() {
 }
 
 fn parse_records_from_excel() {
-match read_excel::<Data>("map_inventaire.xlsx") {
-    Ok(records) => {
-        for record in records {
-            println!("{:?}; {:?}; {:?}; {:?}; ", record.place, record.obj, record.qte, record.emp);
+    match read_excel::<Data>("map_inventaire.xlsx") {
+        Ok(records) => {
+            for record in records {
+                //TODO print only the rows that are problematic, and skip them
+                //TODO remove the problematic data from the Vec<Data>
+                //TODO add to database the places first, then the objects for each place (make sure to map the places first)
+                println!("{:?}; {:?}; {:?}; {:?}; ", record.place, record.obj, record.qte, record.emp);
+            }
         }
+        Err(e) => eprintln!("Error reading excel file: {}", e),
     }
-    Err(e) => eprintln!("Error reading excel file: {}", e),
-}
 }
