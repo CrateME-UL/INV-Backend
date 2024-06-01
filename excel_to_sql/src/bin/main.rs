0593f43949b36f2effe44679f58cea676317de14
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-06-01 00:37:36 -0400

    INV-50: data added but needs testing

--- excel_to_sql/src/bin/main.rs
+++ excel_to_sql/src/bin/main.rs
@@ -1,5 +1,11 @@
+use std::error::Error;
+
+use dotenv::dotenv;
 use excel_to_sql::read_excel;
 use serde::Deserialize;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::env;
 
 #[derive(Debug, Deserialize)]
 struct Data {
@@ -9,21 +15,139 @@ struct Data {
     emp: String,
 }
 
-fn main() {
-    println!("Hello, world!");
-    parse_records_from_excel();
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+struct ParsedData {
+    place: String,
+    obj: String,
+    qte: i32,
+    emp: String,
 }
 
-fn parse_records_from_excel() {
-    match read_excel::<Data>("map_inventaire.xlsx") {
-        Ok(records) => {
-            for record in records {
-                //TODO print only the rows that are problematic, and skip them
-                //TODO remove the problematic data from the Vec<Data>
-                //TODO add to database the places first, then the objects for each place (make sure to map the places first)
-                println!("{:?}; {:?}; {:?}; {:?}; ", record.place, record.obj, record.qte, record.emp);
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    dotenv().ok();
+
+    // get the database URL from the environment variable
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // Create a connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to create pool.");
+
+    let parsed_data = parse_records_from_excel("map_inventaire.xlsx");
+    match parsed_data {
+        Ok(data) => {
+            if let Err(e) = add_places_db(&pool, data.clone()).await {
+                eprintln!("Error adding places to the database: {}", e);
+            }
+            if let Err(e) = add_items_db(&pool, data).await {
+                eprintln!("Error adding items to the database: {}", e);
             }
         }
-        Err(e) => eprintln!("Error reading excel file: {}", e),
+        Err(e) => {
+            eprintln!("Error parsing data: {}", e);
+        }
     }
+}
+
+fn parse_records_from_excel(file_path: &str) -> Result<Vec<ParsedData>, Box<dyn Error>> {
+    let records = read_excel::<Data>(&file_path)?;
+    let max_name_len: usize = env::var("NAME_MAX_LEN")
+        .expect("NAME_MAX_LEN must be set")
+        .parse()
+        .expect("NAME_MAX_LEN must be a valid integer");
+    let mut parsed_data: Vec<ParsedData> = Vec::new();
+    let parse_error_status = -1;
+    let mut i = 0;
+    for record in records {
+        let int_parse_value = match record.qte.parse::<i32>() {
+            Ok(value) => value,
+            Err(_) => parse_error_status,
+        };
+
+        if int_parse_value == parse_error_status
+            || record.obj.len() > max_name_len
+            || record.place.len() > max_name_len
+            || record.emp.len() > 3
+        {
+            i += 1;
+            println!("parsing error: adjust errors and restart after drop tables and recreate table. alternative: add manually {:#?}", record);
+        } else {
+            parsed_data.push(ParsedData {
+                place: record.place,
+                obj: record.obj,
+                qte: int_parse_value,
+                emp: record.emp,
+            });
+        }
+    }
+    println!("nb of parsing errors: {} ", i);
+
+    Ok(parsed_data)
+}
+
+async fn add_places_db(pool: &PgPool, parsed_data: Vec<ParsedData>) -> Result<(), Box<dyn Error>> {
+    let mut i = 0;
+    for parsed_data in parsed_data {
+        let exists: Option<bool> = sqlx::query!(
+            "SELECT EXISTS(SELECT 1 FROM Places WHERE placeName = $1)",
+            parsed_data.place
+        )
+        .fetch_one(pool)
+        .await?
+        .exists;
+
+        if exists.unwrap_or(false) != true {
+            sqlx::query!(
+                "INSERT INTO Places (placeName, placeType) VALUES ($1, $2)",
+                parsed_data.place,
+                parsed_data.emp,
+            )
+            .execute(pool)
+            .await?;
+            i += 1;
+        }
+    }
+    println!("nb of places added: {} ", i);
+    Ok(())
+}
+
+async fn add_items_db(pool: &PgPool, parsed_data: Vec<ParsedData>) -> Result<(), Box<dyn Error>> {
+    let mut i = 0;
+    let absent = -1;
+    for parsed_data in parsed_data {
+        let place_id: Option<i32> = sqlx::query!(
+            "SELECT placeId FROM Places WHERE placeName = $1",
+            parsed_data.place
+        )
+        .fetch_optional(pool)
+        .await?
+        .map(|record| record.placeid);
+
+        let item_id: Option<i32> = sqlx::query!(
+            "SELECT itemId FROM Items WHERE itemName = $1 AND placeId = $2",
+            parsed_data.obj,
+            place_id.unwrap_or(absent),
+        )
+        .fetch_optional(pool)
+        .await?
+        .map(|record| record.itemid);
+
+        if place_id.unwrap_or(absent) != absent && item_id.unwrap_or(absent) == absent {
+            sqlx::query!(
+                "INSERT INTO Items (placeId, nbOfItems, itemName) VALUES ($1, $2, $3)",
+                place_id,
+                parsed_data.qte,
+                parsed_data.obj,
+            )
+            .execute(pool)
+            .await?;
+            i += 1;
+        }
+    }
+    println!("nb of items added: {} ", i);
+    Ok(())
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,12 +7,13 @@
 use axum::{
     http::StatusCode,
     response::IntoResponse,
-    routing::{get, post},
+    routing::get,
     Json, Router,
 };
 
+use uuid::Uuid;
 use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use serde_json::{Map, Number, Value};
 use sqlx::postgres::PgPoolOptions;
 use std::env;
@@ -56,10 +57,6 @@ async fn main() {
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
-        // `GET /users` goes to `user_id`
-        .route("/users", get(get_users))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user))
         // 'get_/places' goes to `get_places`
         .route("/places", get(get_places))
         // 'get_/items' goes to `get_items`
@@ -101,21 +98,6 @@ async fn root() -> &'static str {
 //     (StatusCode::CREATED, Json(item))
 // }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> impl IntoResponse {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
 
 async fn get_places_db() -> anyhow::Result<Vec<Place>> {
     dotenv().ok();
@@ -147,19 +129,19 @@ async fn get_items_db() -> anyhow::Result<Vec<Item>> {
         .max_connections(5)
         .connect(&database_url)
         .await?;
-    let items = sqlx::query_as("SELECT itemId, placeId, nbOfItems, itemName FROM Items")
-        .fetch_all(&pool)
-        .await?;
+    let items = sqlx::query_as(
+        "SELECT itemName, SUM(nbOfItems) as nbOfItems FROM Items GROUP BY itemName"
+    )
+    .fetch_all(&pool)
+    .await?;
     Ok(items
         .into_iter()
-        .map(|item: (i32, i32, i32, String)| Item {
-            item_id: item.0,
-            place_id: item.1,
-            nb_of_items: item.2,
-            item_name: item.3,
+        .map(|item: (String, i64)| Item {
+            item_id: uuid::Uuid::new_v4(),
+            item_name: item.0,
+            nb_of_items: item.1 as i32,
         })
         .collect())
-
 }
 
 
@@ -197,15 +179,14 @@ async fn get_items() -> impl IntoResponse {
                 .into_iter()
                 .map(|item| {
                     let mut map = Map::new();
-                    map.insert("item_id".to_string(), Value::Number(Number::from(item.item_id)));
-                    map.insert("place_id".to_string(), Value::Number(Number::from(item.place_id)));
-                    map.insert("nb_of_items".to_string(), Value::Number(Number::from(item.nb_of_items)));
+                    map.insert("item_id".to_string(), Value::String(item.item_id.to_string()));
                     map.insert("item_name".to_string(), Value::String(item.item_name));
+                    map.insert("nb_of_items".to_string(), Value::Number(Number::from(item.nb_of_items)));
                     Value::Object(map)
                 })
                 .collect();
             let obj = Value::Array(list_response);
-            (StatusCode::CREATED, Json(obj))
+            (StatusCode::OK, Json(obj))
         }
         Err(err) => {
             eprintln!("Error fetching items: {}", err);
@@ -214,72 +195,10 @@ async fn get_items() -> impl IntoResponse {
         }
     }
 }
-async fn get_users_db() -> anyhow::Result<Vec<User>> {
-    // load environment variables from.env file
-    dotenv().ok();
 
-    // get the database URL from the environment variable
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    // Create a connection pool
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&database_url)
-        .await?;
-
-    let users = sqlx::query_as("SELECT id, username FROM users")
-        .fetch_all(&pool) // Use the pool to get a connection that implements the Executor trait
-        .await?;
-
-    Ok(users
-        .into_iter()
-        .map(|user: (i32, String)| User {
-            id: user.0 as u64,
-            username: user.1,
-        })
-        .collect())
-}
-
-
-async fn get_users() -> impl IntoResponse {
-    match get_users_db().await {
-        Ok(users_result) => {
-            let list_response: Vec<Value> = users_result
-                .into_iter()
-                .map(|user| {
-                    let mut map = Map::new();
-                    map.insert("id".to_string(), Value::Number(Number::from(user.id)));
-                    map.insert("username".to_string(), Value::String(user.username));
-                    Value::Object(map)
-                })
-                .collect();
-
-            let obj = Value::Array(list_response);
-
-            // this will be converted into a JSON response
-            // with a status code of `201 Created`
-            (StatusCode::CREATED, Json(obj))
-        }
-        Err(err) => {
-            // Handle the error and return a default response
-            eprintln!("Error fetching users: {}", err);
-            let error_response = serde_json::json!({ "error": err.to_string() });
-            (StatusCode::INTERNAL_SERVER_ERROR, Json(error_response))
-        }
-    }
-}
 
 // the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
 
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
 // #[derive(Deserialize)]
 // struct CreatePlace {
 //     place_id: u32,
@@ -304,10 +223,8 @@ struct Place {
 
 #[derive(Serialize)]
 struct Item {
-    item_id: i32,
-    place_id: i32,
-    nb_of_items: i32,
+    item_id: Uuid,
     item_name: String,
+    nb_of_items: i32,
 }
-
 
