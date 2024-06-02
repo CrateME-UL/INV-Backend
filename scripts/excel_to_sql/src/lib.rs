2429d1466b4b8b48e79a107a90047fdbf124adfd
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-06-02 00:49:38 -0400

    INV-50: :package: refactor code

---
+++ scripts/excel_to_sql/src/lib.rs
@@ -0,0 +1,7 @@
+//* the module api to call */
+mod data_handling;
+
+//* crates that will be available to use in excel_to_sql (root) scope*/
+pub use crate::data_handling::data_parser::{parse_records_from_excel, Data, ParsedData};
+pub use crate::data_handling::excel_reader::read_excel;
+pub use crate::data_handling::sql_handler::{add_places_db, add_items_db};
