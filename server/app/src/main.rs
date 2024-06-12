4b91edf6dcd33eca721daed6542fab8c46841b7f
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-06-12 19:27:43 -0400

    add tests

--- server/app/src/main.rs
+++ server/app/src/main.rs
@@ -1,5 +1,4 @@
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Extension, Json, Router};
-
 use dotenv::dotenv;
 use serde::Serialize;
 use serde_json::{Map, Number, Value};
@@ -7,7 +6,7 @@ use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::env;
 use tower_http::cors::CorsLayer;
 use tracing::instrument;
-use uuid::Uuid;
+use uuid::Uuid; // Import to_bytes function
 
 #[tokio::main(flavor = "current_thread")]
 #[instrument]
@@ -86,4 +85,66 @@ struct Item {
     item_id: Uuid,
     item_name: String,
     nb_of_items: i32,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::body::Body;
+    use axum::http::{Request, StatusCode};
+    use serde_json::Value;
+    use tower::ServiceExt; // Import to_bytes function
+
+    #[tokio::test]
+    async fn test_root() {
+        let app = Router::new().route("/", get(root));
+
+        let response = app.oneshot(Request::new(Body::empty())).await.unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = axum::body::to_bytes(response.into_body(), 1024)
+            .await
+            .unwrap();
+        assert_eq!(body, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn test_get_items() {
+        // Setup database connection and pool
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&database_url)
+            .await
+            .unwrap();
+
+        let app = Router::new()
+            .route("/items", get(get_items))
+            .layer(Extension(pool));
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/items")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = axum::body::to_bytes(response.into_body(), 10000)
+            .await
+            .unwrap();
+        let items: Vec<Value> = serde_json::from_slice(&body).unwrap();
+
+        for item in items {
+            assert!(item.get("item_id").is_some());
+            assert!(item.get("item_name").is_some());
+            assert!(item.get("nb_of_items").is_some());
+        }
+    }
 }
