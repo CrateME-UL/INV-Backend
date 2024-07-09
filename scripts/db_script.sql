b3424bb3d2983646c1eb5da527e768bae2ca7e5d
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-07-08 23:44:09 -0400

    INV-110: modify bd schemas
    Co-authored-by: Axiomemath <Axiomemath@users.noreply.github.com>

--- scripts/db_script.sql
+++ scripts/db_script.sql
@@ -1,18 +1,67 @@
- DROP TABLE IF EXISTS Items;
- DROP TABLE IF EXISTS Places;
+DROP TABLE IF EXISTS Inventory;
+DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS Places;
 
 CREATE TABLE IF NOT EXISTS Places (
-    placeId SERIAL PRIMARY KEY,
-    placeName VARCHAR(30) NOT NULL,
-    placeType VARCHAR(3) NOT NULL
+    place_id SERIAL PRIMARY KEY,
+    place_name VARCHAR(30) UNIQUE NOT NULL,
+    place_type VARCHAR(3) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Items (
-    itemId SERIAL PRIMARY KEY,
-    placeId INT REFERENCES Places(placeId) NOT NULL,
-    nbOfItems INT not NULL, 
-    itemName VARCHAR(30) NOT NULL
+    item_id SERIAL PRIMARY KEY,
+    item_name VARCHAR(30) UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Inventory (
+    place_id INT NOT NULL,
+    item_id INT NOT NULL,
+    nb_of_items INT NOT NULL CHECK (nb_of_items > 0),
+    PRIMARY KEY (place_id, item_id),
+    FOREIGN KEY (place_id) REFERENCES Places(place_id),
+    FOREIGN KEY (item_id) REFERENCES Items(item_id)
+);
+
+INSERT INTO Places (place_name, place_type) VALUES ('Dépôt', 'INV');
+INSERT INTO Places (place_name, place_type) VALUES ('Vachon', 'IN');
+INSERT INTO Places (place_name, place_type) VALUES ('Pouliot', 'OUT');
+
+INSERT INTO Items (item_name) VALUES ('tapis');
+INSERT INTO Items (item_name) VALUES ('chaise');
+INSERT INTO Items (item_name) VALUES ('ballon');
+
+INSERT INTO Inventory (place_id, item_id, nb_of_items)
+SELECT (SELECT place_id FROM Places LIMIT 1), --dépot
+       (SELECT item_id FROM Items LIMIT 1), --tapis
+       2;
+INSERT INTO Inventory (place_id, item_id, nb_of_items)
+SELECT (SELECT place_id FROM Places LIMIT 1 OFFSET 2), --Pouliot 
+       (SELECT item_id FROM Items LIMIT 1 OFFSET 1), --chaise
+       2;
+INSERT INTO Inventory (place_id, item_id, nb_of_items)
+SELECT (SELECT place_id FROM Places LIMIT 1 OFFSET 2), -- Pouliot
+       (SELECT item_id FROM Items LIMIT 1), --tapis
+       3;
+
+-- inventory of places filter by item
+SELECT Places.place_id, Places.place_name, Places.place_type, Inventory.nb_of_items
+FROM Inventory
+JOIN Places ON Inventory.place_id = Places.place_id
+JOIN Items ON Inventory.item_id = Items.item_id
+WHERE item_name = 'tapis'
+ORDER BY Inventory.nb_of_items DESC;
+
+SELECT item_id, item_name FROM Items ORDER BY item_name;
+
+
+-- inventory of items filtered by place
+SELECT Items.item_id, Items.item_name, Inventory.nb_of_items
+FROM Inventory
+JOIN Places ON Inventory.place_id = Places.place_id
+JOIN Items ON Inventory.item_id = Items.item_id
+WHERE place_name = 'Pouliot'
+ORDER BY Inventory.nb_of_items DESC;
+
+SELECT place_id, place_name, place_type FROM Places ORDER BY place_name;
 
 
