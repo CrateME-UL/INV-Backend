a2899e2b9ee6e068634f2625bfa203b62c44418f
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-07-11 00:33:10 -0400

    INV-110: fix calls to inventory
    Co-authored-by: Axiomemath <Axiomemath@users.noreply.github.com>

--- scripts/db_script.sql
+++ scripts/db_script.sql
@@ -22,46 +22,46 @@ CREATE TABLE IF NOT EXISTS Inventory (
     FOREIGN KEY (item_id) REFERENCES Items(item_id)
 );
 
-INSERT INTO Places (place_name, place_type) VALUES ('Dépôt', 'INV');
-INSERT INTO Places (place_name, place_type) VALUES ('Vachon', 'IN');
-INSERT INTO Places (place_name, place_type) VALUES ('Pouliot', 'OUT');
+-- INSERT INTO Places (place_name, place_type) VALUES ('Dépôt', 'INV');
+-- INSERT INTO Places (place_name, place_type) VALUES ('Vachon', 'IN');
+-- INSERT INTO Places (place_name, place_type) VALUES ('Pouliot', 'OUT');
 
-INSERT INTO Items (item_name) VALUES ('tapis');
-INSERT INTO Items (item_name) VALUES ('chaise');
-INSERT INTO Items (item_name) VALUES ('ballon');
+-- INSERT INTO Items (item_name) VALUES ('tapis');
+-- INSERT INTO Items (item_name) VALUES ('chaise');
+-- INSERT INTO Items (item_name) VALUES ('ballon');
 
-INSERT INTO Inventory (place_id, item_id, nb_of_items)
-SELECT (SELECT place_id FROM Places LIMIT 1), --dépot
-       (SELECT item_id FROM Items LIMIT 1), --tapis
-       2;
-INSERT INTO Inventory (place_id, item_id, nb_of_items)
-SELECT (SELECT place_id FROM Places LIMIT 1 OFFSET 2), --Pouliot 
-       (SELECT item_id FROM Items LIMIT 1 OFFSET 1), --chaise
-       2;
-INSERT INTO Inventory (place_id, item_id, nb_of_items)
-SELECT (SELECT place_id FROM Places LIMIT 1 OFFSET 2), -- Pouliot
-       (SELECT item_id FROM Items LIMIT 1), --tapis
-       3;
+-- INSERT INTO Inventory (place_id, item_id, nb_of_items)
+-- SELECT (SELECT place_id FROM Places LIMIT 1), --dépot
+--        (SELECT item_id FROM Items LIMIT 1), --tapis
+--        2;
+-- INSERT INTO Inventory (place_id, item_id, nb_of_items)
+-- SELECT (SELECT place_id FROM Places LIMIT 1 OFFSET 2), --Pouliot 
+--        (SELECT item_id FROM Items LIMIT 1 OFFSET 1), --chaise
+--        2;
+-- INSERT INTO Inventory (place_id, item_id, nb_of_items)
+-- SELECT (SELECT place_id FROM Places LIMIT 1 OFFSET 2), -- Pouliot
+--        (SELECT item_id FROM Items LIMIT 1), --tapis
+--        3;
 
--- inventory of places filter by item
-SELECT Places.place_id, Places.place_name, Places.place_type, Inventory.nb_of_items
-FROM Inventory
-JOIN Places ON Inventory.place_id = Places.place_id
-JOIN Items ON Inventory.item_id = Items.item_id
-WHERE item_name = 'tapis'
-ORDER BY Inventory.nb_of_items DESC;
+-- -- inventory of places filter by item
+-- SELECT Places.place_id, Places.place_name, Places.place_type, Inventory.nb_of_items
+-- FROM Inventory
+-- JOIN Places ON Inventory.place_id = Places.place_id
+-- JOIN Items ON Inventory.item_id = Items.item_id
+-- WHERE item_name = 'tapis'
+-- ORDER BY Inventory.nb_of_items DESC;
 
-SELECT item_id, item_name FROM Items ORDER BY item_name;
+-- SELECT item_id, item_name FROM Items ORDER BY item_name;
 
 
--- inventory of items filtered by place
-SELECT Items.item_id, Items.item_name, Inventory.nb_of_items
-FROM Inventory
-JOIN Places ON Inventory.place_id = Places.place_id
-JOIN Items ON Inventory.item_id = Items.item_id
-WHERE place_name = 'Pouliot'
-ORDER BY Inventory.nb_of_items DESC;
+-- -- inventory of items filtered by place
+-- SELECT Items.item_id, Items.item_name, Inventory.nb_of_items
+-- FROM Inventory
+-- JOIN Places ON Inventory.place_id = Places.place_id
+-- JOIN Items ON Inventory.item_id = Items.item_id
+-- WHERE place_name = 'Pouliot'
+-- ORDER BY Inventory.nb_of_items DESC;
 
-SELECT place_id, place_name, place_type FROM Places ORDER BY place_name;
+-- SELECT place_id, place_name, place_type FROM Places ORDER BY place_name;
 
 
--- server/app/src/main.rs
+++ server/app/src/main.rs
@@ -195,26 +195,47 @@ async fn get_inventory_items_db(
     query: &Query<InventoryItemQuery>,
 ) -> Result<Vec<InventoryItem>, Box<dyn std::error::Error>> {
     let default = "";
-    let items = sqlx::query!(
-        "SELECT Items.item_id as item_id, Items.item_name as item_name, Inventory.nb_of_items as nb_of_items
-            FROM Inventory
-            JOIN Places ON Inventory.place_id = Places.place_id
-            JOIN Items ON Inventory.item_id = Items.item_id
-            WHERE (place_name =  $1 OR $1 = '') 
-                AND (place_type = $2 OR $2 = '')
-            ORDER BY Inventory.nb_of_items DESC;",
+    let is_query_empty = query.place_name.as_deref().unwrap_or(default) == default
+        && query.place_type.as_deref().unwrap_or(default) == default;
+    let items = match is_query_empty {
+        true => sqlx::query!(
+            "SELECT Items.item_id, Items.item_name, SUM (Inventory.nb_of_items) AS nb_of_items
+                    FROM Inventory
+                    JOIN Places ON Inventory.place_id = Places.place_id
+                    JOIN Items ON Inventory.item_id = Items.item_id
+                GROUP BY Items.item_id, Items.item_name 
+                ORDER BY nb_of_items DESC, Items.item_name;"
+        )
+        .fetch_all(pool)
+        .await?
+        .into_iter()
+        .map(|record| InventoryItem {
+            item_id: record.item_id,
+            item_name: record.item_name,
+            nb_of_items: record.nb_of_items.unwrap_or(0) as i32,
+        })
+        .collect(),
+        _ => sqlx::query!(
+            "SELECT Items.item_id, Items.item_name, Inventory.nb_of_items
+                    FROM Inventory
+                    JOIN Places ON Inventory.place_id = Places.place_id
+                    JOIN Items ON Inventory.item_id = Items.item_id
+                    WHERE (place_name =  $1 OR $1 = '') 
+                        AND (place_type = $2 OR $2 = '')
+                    ORDER BY Inventory.nb_of_items DESC;",
             query.place_name.as_deref().unwrap_or(default),
             query.place_type.as_deref().unwrap_or(default),
-    )
-    .fetch_all(pool)
-    .await?
-    .into_iter()
-    .map(|record| InventoryItem {
-        item_id: record.item_id,
-        item_name: record.item_name,
-        nb_of_items: record.nb_of_items,
-    })
-    .collect();
+        )
+        .fetch_all(pool)
+        .await?
+        .into_iter()
+        .map(|record| InventoryItem {
+            item_id: record.item_id,
+            item_name: record.item_name,
+            nb_of_items: record.nb_of_items,
+        })
+        .collect(),
+    };
 
     Ok(items)
 }
@@ -224,25 +245,50 @@ async fn get_inventory_places_db(
     query: &Query<InventoryPlaceQuery>,
 ) -> Result<Vec<InventoryPlace>, Box<dyn std::error::Error>> {
     let default = "";
-    let places = sqlx::query!(
-        "SELECT Places.place_id as place_id, Places.place_name as place_name, Places.place_type as place_type, Inventory.nb_of_items as nb_of_items
-            FROM Inventory
-            JOIN Places ON Inventory.place_id = Places.place_id
-            JOIN Items ON Inventory.item_id = Items.item_id
-            WHERE (item_name =  $1 OR $1 = '') 
-            ORDER BY Inventory.nb_of_items DESC;",
-        query.item_name.as_deref().unwrap_or(default),
-    )
-    .fetch_all(pool)
-    .await?
-    .into_iter()
-    .map(|record| InventoryPlace {
-        place_id: record.place_id,
-        place_name: record.place_name,
-        place_type: record.place_type,
-        nb_of_items: record.nb_of_items,
-    })
-    .collect();
+    let is_query_empty = query.item_name.as_deref().unwrap_or(default) == default;
+    let places = match is_query_empty {
+        true => {
+            sqlx::query!(
+                "SELECT Places.place_id, Places.place_name, Places.place_type, SUM(Inventory.nb_of_items) as nb_of_items
+                    FROM Inventory
+                    JOIN Places ON Inventory.place_id = Places.place_id
+                    JOIN Items ON Inventory.item_id = Items.item_id
+                GROUP BY Places.place_id, Places.place_name, Places.place_type
+                ORDER BY nb_of_items DESC, Places.place_name;"
+            )
+            .fetch_all(pool)
+            .await?
+            .into_iter()
+            .map(|record| InventoryPlace {
+                place_id: record.place_id,
+                place_name: record.place_name,
+                place_type: record.place_type,
+                nb_of_items: record.nb_of_items.unwrap_or(0) as i32,
+            })
+            .collect()
+        },
+        _ => {
+            sqlx::query!(
+                "SELECT Places.place_id as place_id, Places.place_name as place_name, Places.place_type as place_type, Inventory.nb_of_items as nb_of_items
+                    FROM Inventory
+                    JOIN Places ON Inventory.place_id = Places.place_id
+                    JOIN Items ON Inventory.item_id = Items.item_id
+                    WHERE (item_name =  $1 OR $1 = '') 
+                    ORDER BY Inventory.nb_of_items DESC;",
+                query.item_name.as_deref().unwrap_or(default),
+            )
+            .fetch_all(pool)
+            .await?
+            .into_iter()
+            .map(|record| InventoryPlace {
+                place_id: record.place_id,
+                place_name: record.place_name,
+                place_type: record.place_type,
+                nb_of_items: record.nb_of_items,
+            })
+            .collect()
+        }
+    };
 
     Ok(places)
 }
